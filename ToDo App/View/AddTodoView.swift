c8900e5e87cc86a52227ac5203364aa0a6987752
Ahnaf2727 <captain.ahnaf27@gmail.com>
2021-02-15 11:11:43 +0700

    Hari Pertama

---
+++ ToDo App/View/AddTodoView.swift
@@ -0,0 +1,20 @@
+//
+//  AddTodoView.swift
+//  ToDo App
+//
+//  Created by MacBook on 15/02/21.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTodoView()
+    }
+}
